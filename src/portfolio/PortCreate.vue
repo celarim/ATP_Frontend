c6b37378a73106e1eb83aa86ce876e2f5579b0b7
bdt6246 <a01094079599@gmail.com>
2024-12-24 17:52:37 +0900

    [feautres] 포트폴리오 생성 페이지 vue 수정정

--- src/portfolio/PortCreate.vue
+++ src/portfolio/PortCreate.vue
@@ -1,9 +1,18 @@
 <script setup>
-import {ref,reactive} from 'vue';
-const addCount = 1;
+import {ref,reactive, computed } from 'vue';
+const addCount = ref(1);
 const addBtn = () => {
-    addCount++;
+    addCount.value++;
 }
+
+const stockData = reactive({
+    name:'',
+    quantity : 0,
+    price:0,
+})
+
+const sum = computed(() => stockData.quantity * stockData.price);
+
 
 </script>
 
@@ -21,32 +30,24 @@ const addBtn = () => {
             <div class="field-labels">
                 <div class="stock-name-label">Stock</div>
                 <div class="quantity-label">Quantity</div>
-                <div class="amount-label">Amount</div>
+                <div class="amount-label">Price</div>
                 <div class="date-label">Date</div>
                 <div class="count-label">Count</div>
-                <div class="add-label"></div> <!-- Add 버튼 자리 --> 
             </div>
 
             <!-- Field Inputs Row -->
             <div id="field-wrapper">
                 <!-- Initial Field Container -->
                 <div class="field-container" v-for = "list in addCount" >
-                    <div class="field-input stock-name-input">
-                        <input type="text" placeholder="Enter stock name" />
-                    </div>
-                    <div class="field-input quantity-input">
-                        <input type="number" placeholder="0" />
-                    </div>
-                    <div class="field-input date-input">
-                        <button class="date-button">
-                            <img src="../images/calendar.svg" alt="Calendar" />
-                        </button>
-                        <input type="date" class="hidden-date-picker" />
-                    </div>
-                    <!-- Add Button -->
-                    <div class="field-input add-field">
-                        <button @click="addBtn" class="add-field-button" id="add-field-button" type="button">+</button>
-                    </div>
+                    <input type="text" placeholder="Enter stock name" v-model="stockData.name"/>
+                    <input type="number" placeholder="0" v-model="stockData.quantity"/>
+                    <input type="number" placeholder="0" v-model="stockData.price"/>
+                    <button class="date-button"><img src="../images/calendar.svg" alt="Calendar" /></button>
+                    <input type="date" class="hidden-date-picker" />
+                    <p>{{ sum }}</p>
+                </div>
+                <div class="field-input add-field">
+                    <button @click="addBtn" class="add-field-button" id="add-field-button" type="button">+</button>
                 </div>
             </div>
         </div>
