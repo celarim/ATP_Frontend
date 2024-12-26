3476b1ee73304f13b50547ca686013eb83f25cf2
Xen-alpha <hky7548@naver.com>
2024-12-26 14:22:36 +0900

    [Fix] fix wrong module import directory

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 import router from './router';
 import Nav from './common/Navbar.vue'
-import stockList from './components/stock/stockList.vue'
+import stockList from './stock/stockList.vue'
 </script>
 
 <template>
 
   <Nav></Nav>
-  
+
   <router-view></router-view>
 
 </template>
