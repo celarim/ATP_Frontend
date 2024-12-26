5029bf6fa5322abe7b9c0a965d668e2308eccbc0
celarim <k6jun@kookmin.ac.kr>
2024-12-26 15:02:56 +0900

    [Feat] 임시 Login Vue
    임시 로그인 뷰를 제작
    아무 기능 없음음

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,15 @@ import PortfolioDetail from '../portfolio/PortfolioDetail.vue';
 import StockDetail from '../stock/StockDetail.vue';
 import CreateP from '../portfolio/PortCreate.vue'
 import StockList from '../stock/StockList.vue';
+import Login from '../user/Login.vue';
 
 const routes = [
   { path: '/', component: Main },
   { path: '/create_p', component: CreateP},
   { path: '/portfolio/:idx', component: PortfolioDetail},
   { path: '/stock/:idx', component: StockDetail},
-  { path: '/stockList', component: StockList}
+  { path: '/stockList', component: StockList},
+  { path: '/login', component: Login}
 ]
 
 const router = createRouter({
---
+++ src/user/Login.vue
@@ -0,0 +1,92 @@
+<script setup>
+
+</script>
+
+<template>
+<sb-login>
+    <sb-layout-auth class="bg-primary">
+        <div id="layoutAuthentication" class="bg-primary">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <sbpro-page-card size="large" style="display: flex;justify-content: center;">
+                                <div class="col-xl-10 col-lg-12">
+                                    <div class="main-card" >
+                                        <div class="card-body under">
+                                            <div class="row justify-content-center">
+                                                <div class="col-xl-8 col-lg-9">
+                                                    <sbpro-login-form><!----><!----><!---->
+                                                        <form novalidate=""class="ng-untouched ng-pristine ng-invalid">
+                                                            <div class="mb-3"><label for="emailInput"
+                                                                    class="text-gray-600 small">Email
+                                                                    address</label><input id="emailInput"
+                                                                    data-cy="emailInput" type="email"
+                                                                    aria-describedby="emailHelp"
+                                                                    formcontrolname="email"
+                                                                    class="input-control form-control-solid ng-untouched ng-pristine ng-invalid">
+                                                                <div class="invalid-feedback">Email required.
+                                                                </div><!----><!---->
+                                                            </div>
+                                                            <div class="mb-3"><label for="passwordInput"
+                                                                    class="small text-gray-600">Password</label><input
+                                                                    id="passwordInput" data-cy="passwordInput"
+                                                                    type="password" formcontrolname="password"
+                                                                    class="input-control form-control-solid ng-untouched ng-pristine ng-invalid">
+                                                                <div class="invalid-feedback">Password required.
+                                                                </div><!----><!---->
+                                                            </div>
+                                                            <div class="mb-3">
+                                                                <a routerlink="/auth/password-reset-request"
+                                                                    href="/auth/password-reset-request">Forgot
+                                                                    your password?</a>
+                                                            </div>
+                                                            <div
+                                                                class="mb-3 d-flex align-items-center justify-content-between">
+                                                                <div
+                                                                    class="form-check form-check-solid custom-checkbox">
+                                                                    <input id="checkInput" data-cy="checkInput"
+                                                                        type="checkbox" formcontrolname="checkbox"
+                                                                        class="form-check-input small ng-untouched ng-pristine ng-valid"><label
+                                                                        for="checkInput"
+                                                                        class="form-check-label">Remember
+                                                                        password</label>
+                                                                </div><button data-cy="loginButton" type="submit"
+                                                                    class="btn btn-primary"
+                                                                    disabled="">Login</button>
+                                                            </div>
+                                                        </form>
+                                                        <p class="small pt-3">This
+                                                            site is protected by reCAPTCHA and the Google
+                                                            <a href="https://policies.google.com/privacy"
+                                                                target="_blank" rel="noreferrer">Privacy Policy
+                                                            </a>and <a href="https://policies.google.com/terms"
+                                                                target="_blank" rel="noreferrer">Terms of
+                                                                Service </a>apply.
+                                                        </p><!----><!---->
+                                                    </sbpro-login-form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <hr class="my-0">
+                                        <div class="card-body px-5 py-4">
+                                            <div class="small text-center">New user?
+                                                <a routerlink="../register" href="/auth/register">Create an
+                                                    account!</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </sbpro-page-card>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </sb-layout-auth>
+</sb-login>
+</template>
+
+<style scoped>
+    @import './login.css';
+</style>
